093e51b42f8d4fe370a6327f671aaf9deb6ee94f
budou114 <58349030+budou114@users.noreply.github.com>
2022-10-22 10:05:05 +0900

    7. Store・Action・Reducerを実際にコーディングして用意しよう

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 
 // Actions -> increment, decrement
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
 
-// reducer
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+// Reducer -> アクションと前の状態を組み合わせて新しい状態に更新する。
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
 
 // Store
+let store = createStore(countReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
